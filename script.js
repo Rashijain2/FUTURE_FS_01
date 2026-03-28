d7babee8a0fc2ef9aac76cbc8c22da5756300db0
Rashijain2 <168710698+Rashijain2@users.noreply.github.com>
2026-03-28 16:38:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function scrollToSection(id) {
+  document.getElementById(id).scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+// Typing effect
+const text = "Full Stack Developer | AI Enthusiast";
+let i = 0;
+
+function typing() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing, 50);
+  }
+}
+
+typing();
